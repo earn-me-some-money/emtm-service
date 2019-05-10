3c58b018da1195eea2a479a85e5c159ec139b7a5
Miguel Chan <vvchan@outlook.com>
2019-05-10 14:52:03 +0800

    Switch to actix-web framework, dropping Rocket

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # emtm-service
 
 Server Repository for Earn Me The Money Project
+
+## Use
+
+- Build: `cargo build`
+- Run: `cargo run`
+
+## Contributing Guideline
+When you are making a commit, please make sure:
+
+1. Use `cargo fmt` to format your code first.
+2. Make sure your code are commented.
+3. Use pull request and make sure your code is reviewed.
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+extern crate env_logger;
+use actix_web::{get, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use futures::IntoFuture;
 
-#[macro_use] extern crate rocket;
+extern crate num_cpus;
 
 #[get("/")]
-fn index() -> &'static str {
+fn index(req: HttpRequest) -> &'static str {
     "Hello, world!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+fn index_async(req: HttpRequest) -> impl IntoFuture<Item = &'static str, Error = Error> {
+    println!("REQ: {:?}", req);
+    Ok("Hello async request!\r\n")
+}
+
+#[get("/{name}")]
+fn hello_name(req: HttpRequest, name: web::Path<String>) -> String {
+    format!("Hello, {}!\r\n", name)
+}
+
+fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::DefaultHeaders::new().header("X-Version", "0.2"))
+            .wrap(middleware::Compress::default())
+            .wrap(middleware::Logger::default())
+            .service(index)
+            .service(hello_name)
+            .service(
+                web::resource("/async/")
+                    .wrap(middleware::DefaultHeaders::new().header("X-Version-R2", "0.3"))
+                    .default_service(web::route().to(|| HttpResponse::MethodNotAllowed()))
+                    .route(web::get().to_async(index_async)),
+            )
+            .service(web::resource("/res/test1.html").to(|| "Test\r\n"))
+    })
+    .bind("127.0.0.1:9876")?
+    .workers(num_cpus::get())
+    .run()
 }
